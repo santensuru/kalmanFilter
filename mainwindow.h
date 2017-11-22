f76daa36ac97b61cd046d156426fb8bd36b57e96
Djuned Fernando Djusdek <santensuru@users.noreply.github.com>
2017-11-22 17:24:58 +0900

    using Qt main Window

--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,31 @@
 
 #include <QMainWindow>
 
+// Module "core"
+#include <opencv2/core/core.hpp>
+
+// Module "highgui"
+#include <opencv2/highgui/highgui.hpp>
+
+// Module "imgproc"
+#include <opencv2/imgproc/imgproc.hpp>
+
+// Module "video"
+#include <opencv2/video/video.hpp>
+
+// Output
+#include <iostream>
+
+// Vector
+#include <vector>
+
+using namespace std;
+
+// >>>>> Color to be tracked
+#define MIN_H_BLUE 200
+#define MAX_H_BLUE 300
+// <<<<< Color to be tracked
+
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +40,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void execute();
+
 private:
     Ui::MainWindow *ui;
+
+    void runKalmanFilter();
 };
 
 #endif // MAINWINDOW_H
